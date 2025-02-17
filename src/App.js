d828916a90f5a23866cb0dc1230e26373ca88462
Rahul A <rahulanil600@gmail.com>
2025-02-17 15:09:30 +0530

    code change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import {useSelector ,useDispatch} from 'react-redux'
 import { increment,decrement } from './Action';
-
+//changed to 1
 function App() {
   const counter = useSelector(state=>state.counter)
   const dispatch = useDispatch();
   return (
     <div className="App">
      <h1>Hello {counter}</h1>
-     <button onClick={()=>dispatch(increment())}>+</button>
-     <button onClick={()=>dispatch(decrement())}>-</button>
+     <button onClick={()=>dispatch(increment())}>Add</button>
+     <button onClick={()=>dispatch(decrement())}>Reduce</button>
      
     </div>
   );
